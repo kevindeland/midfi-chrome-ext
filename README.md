2694a5a880f1b19d7ef0cdcefe8e1a9dc8741310
Kevin DeLand <Kevin DeLand>
2017-06-01 13:49:32 -0400

    chrome extension ready for use

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Midfi prototyping chrome extension
+This is a chrome extension for performing midfi prototyping with a chrome extension
+
+## Instructions
+
+
+## Developer notes and process work
+### Fun With z-index
+this guy makes a little badge that overlays the entire page!
+https://css-tricks.com/video-screencasts/40-how-z-index-works/
+1. pick an Image: url(https://www.iconfinder.com/data/icons/black-logistics-icons/256/Robot_head.png)
+2. add a div
+3. give it a css with proper dimensions, absolute position, top: 0px and right: 0px, with background or url(images/badge.png);
+
+### Other resources used
+https://stackoverflow.com/questions/8926378/how-to-select-an-input-element-by-value-using-javascript
+https://www.w3schools.com/jquery/jquery_slide.asp
+
+Adding as chrome plugin:
+https://stackoverflow.com/questions/43029072/run-js-script-in-chrome-extension-on-page-load
--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 
+window.onload = function () {
+  console.log("page loaded, adding script to page");
+  // copy and paste all of these into the console
+  var elemDiv = document.createElement('div');
+  elemDiv.setAttribute("id", "bot");
+  elemDiv.style.cssText= 'position:absolute;width:256px;height:256px;top:0px;right:0px;background: url(https://www.iconfinder.com/data/icons/black-logistics-icons/256/Robot_head.png); display:none;';
+  document.body.appendChild(elemDiv);
 
-// copy and paste all of these into the console
-var elemDiv = document.createElement('div');
-elemDiv.setAttribute("id", "bot");
-elemDiv.style.cssText= 'position:absolute;width:256px;height:256px;top:0px;right:0px;background: url(https://www.iconfinder.com/data/icons/black-logistics-icons/256/Robot_head.png); display:none;';
-document.body.appendChild(elemDiv);
+  function hiRobot() {
+    $("#bot").slideDown();
+  }
 
-function hiRobot() {
-  $('#bot').slideDown();
-}
+  function byeRobot() {
+    $("#bot").slideUp();
+  }
 
-function byeRobot() {
-  $('#bot').slideUp();
-}
+  var calculateGoal = document.querySelectorAll("input[value=\"Calculate Goal\"]")[0];
+  calculateGoal.addEventListener('mouseover', hiRobot);
 
-var calculateGoal = document.querySelectorAll("input[value=\"Calculate Goal\"]")[0];
-calculateGoal.addEventListener('mouseover', hiRobot);
+  calculateGoal.addEventListener('mouseleave', byeRobot);
+}();
 
-calculateGoal.addEventListener('mouseleave', byeRobot);
 
 
 // unused code
